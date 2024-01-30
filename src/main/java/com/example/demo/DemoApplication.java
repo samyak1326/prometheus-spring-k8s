3fc1a3336dfad574af23064e9eb40ae1604da31d
SAMYAK JAIN <jainsamyak107@gmail.com>
2024-01-30 16:26:37 -0500

    fix: dependency changed for Histogram @bean added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
 //import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.Histogram;
+import io.prometheus.metrics.exporter.servlet.jakarta.PrometheusMetricsServlet;
+import io.prometheus.metrics.instrumentation.jvm.JvmMetrics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 
@@ -12,6 +17,12 @@ public class DemoApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+//        JvmMetrics.builder().register();
+//        Histogram.build().register();
     }
 
+    @Bean
+    public ServletRegistrationBean<PrometheusMetricsServlet> createPrometheusMetricsEndpoint() {
+        return new ServletRegistrationBean<>(new PrometheusMetricsServlet(), "/metrics/*");
+    }
 }
--- src/main/java/com/example/demo/WebApp.java
+++ src/main/java/com/example/demo/WebApp.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import io.prometheus.metrics.exporter.servlet.jakarta.PrometheusMetricsServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.databind.JsonNode;
 import io.prometheus.client.Counter;
-import io.prometheus.client.Histogram;
+import io.prometheus.metrics.core.metrics.Histogram;
 import io.prometheus.client.exporter.HTTPServer;
 
 import java.io.IOException;
@@ -21,10 +24,10 @@ public class WebApp {
             .help("Total number of requests to /hello endpoint")
             .register();
 
-    private static final Histogram requestDuration = Histogram.build()
+    private static final Histogram requestDuration = Histogram.builder()
             .name("http_request_duration_ms")
             .help("HTTP requests service time in ms")
-            .buckets(20,40,60,80,100,120,140,160,180,200) // create time buckets at an interval of 20ms.
+            .classicUpperBounds(20,40,60,80,100,120,140,160,180,200) // create time buckets at an interval of 20ms.
             .labelNames("method", "path", "status_code")
             .register();
 
@@ -34,34 +37,34 @@ public class WebApp {
 //            .labelNames("status_code")
 //            .register();
 
-    private static final Histogram requestLatencyForHello = Histogram.build()
-            .name("hello_request_latency_milliseconds")
-            .help("Request latency for Hello requests in milliseconds")
-            .register();
+//    private static final Histogram requestLatencyForHello = Histogram.build()
+//            .name("hello_request_latency_milliseconds")
+//            .help("Request latency for Hello requests in milliseconds")
+//            .register();
 
-    static {
-        try {
-            // Start Prometheus HTTPServer on port 9400
-            HTTPServer server = new HTTPServer(9400);
-            System.out.println("HTTPServer listening on port http://localhost:" + server.getPort() + "/metrics");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//    static {
+//        try {
+//            // Start Prometheus HTTPServer on port 9400
+//            HTTPServer server = new HTTPServer(9400);
+//            System.out.println("HTTPServer listening on port http://localhost:" + server.getPort() + "/metrics");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
-    @GetMapping("/Hello")
-    public String Hello() {
-
-        counter.inc();
-        Histogram.Timer requestTimer = requestLatencyForHello.startTimer();
-        try{
-            Thread.sleep(10);
-            return "Trying to build a Server";
-        }catch(Exception x) {
-            requestTimer.observeDuration(); //observe request duration
-        }
-        return "hello";
-    }
+//    @GetMapping("/Hello")
+//    public String Hello() {
+//
+//        counter.inc();
+//        Histogram.Timer requestTimer = requestLatencyForHello.startTimer();
+//        try{
+//            Thread.sleep(10);
+//            return "Trying to build a Server";
+//        }catch(Exception x) {
+//            requestTimer.observeDuration(); //observe request duration
+//        }
+//        return "hello";
+//    }
 
     @PostMapping("/PostEndpoint")
     public ResponseEntity<String> tryDelay(@RequestBody JsonNode requestBody) {
@@ -77,26 +80,32 @@ public class WebApp {
             long durationInMillis = (System.nanoTime() - start) / 1_000_000; // Convert nanoseconds to milliseconds
             System.out.println("POST request - delay of " + durationInMillis + " ms");
 
-            requestDuration.labels("POST", "/PostEndpoint", "200")
+            requestDuration.labelValues("POST", "/PostEndpoint", "200")
                     .observe(durationInMillis); // Observe in milliseconds
             System.out.println("POST request - delay of " + durationInMillis + " ms");
             return ResponseEntity.ok("Delay processed: " + durationInMillis + " ms");
 
         } catch (NumberFormatException ex) {
             long durationInMillis = (System.nanoTime() - start) / 1_000_000; // Convert nanoseconds to milliseconds
-            requestDuration.labels("POST", "/PostEndpoint", "400")
+            requestDuration.labelValues("POST", "/PostEndpoint", "400")
                     .observe(durationInMillis);
             return ResponseEntity.badRequest().body("Invalid number format for key: " + keyValue);
 
         } catch (InterruptedException ex) {
             long durationInMillis = (System.nanoTime() - start) / 1_000_000; // Convert nanoseconds to milliseconds
 
-            requestDuration.labels("POST", "/PostEndpoint", "500")
+            requestDuration.labelValues("POST", "/PostEndpoint", "500")
                     .observe(durationInMillis);
             Thread.currentThread().interrupt();
             return ResponseEntity.internalServerError().body("Error during thread sleep: " + ex.getMessage());
         }
     }
+
+//    @Bean
+//    public ServletRegistrationBean<PrometheusMetricsServlet> createPrometheusMetricsEndpoint() {
+//        return new ServletRegistrationBean<>(new PrometheusMetricsServlet(), "/metrics/*");
+//    }
+
 }
 
 
